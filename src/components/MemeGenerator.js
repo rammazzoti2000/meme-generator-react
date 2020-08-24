8e2c5c4d5238c882098437cece54db9ceb75a897
Alexandru <rammazzoti2000@gmail.com>
2020-08-24 17:13:45 +0200

    Implement a API call for image content

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -7,7 +7,20 @@ class MemeGenerator extends Component {
           topText: '',
           bottomText: '',
           randomImg: 'http://i.imgflip.com/1bij.jpg',
+          allMemeImgs: [],
         }
+    }
+
+    componentDidMount() {
+      fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+        .then(response => {
+          const { memes } = response.data
+          console.log(memes[0]);
+          this.setState({
+            allMemeImgs: memes,
+          })
+        })
     }
 
     render() {
