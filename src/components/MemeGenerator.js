03eb50ee4ce6293207e50041d8e47bf9f5cc6fe4
Alexandru <rammazzoti2000@gmail.com>
2020-08-24 17:02:37 +0200

    Initialize MemeGenerator state with three props

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -3,7 +3,11 @@ import React, {Component} from "react"
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state ={}
+        this.state = {
+          topText: '',
+          bottomText: '',
+          randomImg: 'http://i.imgflip.com/1bij.jpg',
+        }
     }
 
     render() {
