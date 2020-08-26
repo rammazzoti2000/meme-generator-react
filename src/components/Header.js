59162a1e47af55553e35c0305419527032a66433
Alexandru <rammazzoti2000@gmail.com>
2020-08-26 13:17:22 +0200

    feat: Project completed

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="https://www.triklopodia.gr/wp-content/uploads/2018/09/to-facebook-douleuei-gia-ta-surizotrol-_1.jpg"
+        alt="Problem?"
+      />
+      <p>Meme Generator</p>
+    </header>
+  );
+}
+
+export default Header;
